d8d80f11dad3dc3cc1d694c8ef0c3928ba4521e1
younesYD <139397542+younesYD@users.noreply.github.com>
2023-10-28 22:43:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let screen = document.getElementById("screen");
+
+
+function leter(Lt){
+    screen.innerHTML += Lt;
+}
+function space(){
+    screen.innerHTML += " ";
+}
+function Delate(){
+    screen.innerHTML = screen.innerHTML.slice(0,-1);
+}
+function color(clr){
+    document.getElementById('leterDiv1').style.borderColor = clr
+    document.getElementById('leterDiv2').style.borderColor = clr
+    document.getElementById('leterDiv3').style.borderColor = clr
+
+    document.getElementById('leterDiv4').style.borderColor = clr
+    document.getElementById('leterDiv5').style.borderColor = clr
+    document.getElementById('leterDiv6').style.borderColor = clr
+
+    document.getElementById('leterDiv7').style.borderColor = clr
+    document.getElementById('leterDiv8').style.borderColor = clr
+    document.getElementById('leterDiv9').style.borderColor = clr
+
+    document.getElementById('leterDiv10').style.borderColor = clr
+    document.getElementById('leterDiv11').style.borderColor = clr
+    document.getElementById('leterDiv12').style.borderColor = clr
+
+    document.getElementById('leterDiv13').style.borderColor = clr
+    document.getElementById('leterDiv14').style.borderColor = clr
+    document.getElementById('leterDiv15').style.borderColor = clr
+
+    document.getElementById('leterDiv16').style.borderColor = clr
+    document.getElementById('leterDiv17').style.borderColor = clr
+    document.getElementById('leterDiv18').style.borderColor = clr
+
+    document.getElementById('leterDiv19').style.borderColor = clr
+    document.getElementById('leterDiv20').style.borderColor = clr
+    document.getElementById('leterDiv21').style.borderColor = clr
+
+    document.getElementById('leterDiv1').style.borderColor = clr
+    document.getElementById('leterDiv1').style.borderColor = clr
+    document.getElementById('leterDiv1').style.borderColor = clr
+
+    document.getElementById('leterDiv22').style.borderColor = clr
+    document.getElementById('leterDiv23').style.borderColor = clr
+    document.getElementById('leterDiv24').style.borderColor = clr
+
+    document.getElementById('leterDiv25').style.borderColor = clr
+    document.getElementById('leterDiv26').style.borderColor = clr
+    document.getElementById('leterDiv27').style.borderColor = clr
+    document.getElementById('leterDiv28').style.borderColor = clr
+}
